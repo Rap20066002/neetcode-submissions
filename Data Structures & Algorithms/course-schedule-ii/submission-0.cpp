52a199ea25657cb1dae3ac2b7dded5361a00941b
Punjani Rezaabbas <rezaabbaspunjani28@gmail.com>
2026-06-23 04:01:33 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        
+        for (auto &it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+            inDegree[it[0]]++;
+        }     
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+
+            for (auto it : adj[node]) {
+                inDegree[it]--;
+                if (inDegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        if(order.size() == numCourses) return order;
+        else return {};
+    }
+};
